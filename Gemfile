521d16295d45584158bb81b1d197341bed72c98f
Masato Ikeda <masato.ikeda@gmail.com>
2020-07-28 19:53:59 +0900

    Add test codes

--- Gemfile
+++ Gemfile
@@ -24,3 +24,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mongoid'
+gem 'test-unit'
---
+++ test.rb
@@ -0,0 +1,26 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
+
+# Create with symbol field
+
+cat = SymbolAnimal.create(species: :cat)
+assert_equal :cat, cat.species
+
+cat_reloaded = SymbolAnimal.find(cat.id)
+assert_equal :cat, cat_reloaded.species
+
+cat_other = StringAnimal.find(cat.id)
+assert_equal "cat", cat_other.species
+
+
+# Create with string field
+
+dog = StringAnimal.create(species: "dog")
+assert_equal "dog", dog.species
+
+dog_reloaded = StringAnimal.find(dog.id)
+assert_equal "dog", dog_reloaded.species
+
+dog_other = SymbolAnimal.find(dog.id)
+assert_equal :dog, dog_other.species
