018d59a7502d6f7b983c559926f6875e1a968a67
Masato Ikeda <masato.ikeda@gmail.com>
2020-07-28 20:12:58 +0900

    Update tests

--- test.rb
+++ test.rb
@@ -9,12 +9,16 @@ assert_equal :cat, cat.species
 
 cat_reloaded = SymbolAnimal.find(cat.id)
 assert_equal :cat, cat_reloaded.species
+assert_match /species: :cat/, cat_reloaded.inspect
 
 cat_other = StringAnimal.find(cat.id)
 assert_equal "cat", cat_other.species
+assert_match /species: :cat/, cat_other.inspect
 
 cat_other.save!
-assert_equal :cat, SymbolAnimal.find(cat.id).species
+cat_updated = SymbolAnimal.find(cat.id)
+assert_equal :cat, cat_updated.species
+assert_match /species: :cat/, cat_updated.inspect
 
 # Create with string field
 
@@ -23,12 +27,16 @@ assert_equal "dog", dog.species
 
 dog_reloaded = StringAnimal.find(dog.id)
 assert_equal "dog", dog_reloaded.species
+assert_match /species: "dog"/, dog_reloaded.inspect
 
 dog_other = SymbolAnimal.find(dog.id)
 assert_equal :dog, dog_other.species
+assert_match /species: "dog"/, dog_other.inspect
 
 dog_other.save!
-assert_equal "dog", StringAnimal.find(dog.id).species
+dog_updated = StringAnimal.find(dog.id)
+assert_equal "dog", dog_updated.species
+assert_match /species: "dog"/, dog_updated.inspect
 
 # Update symbol to string
 
