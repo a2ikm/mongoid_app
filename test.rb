6723c7f37cf6df0cc026cfe067258f6dbab5c28d
Masato Ikeda <masato.ikeda@gmail.com>
2020-07-29 17:22:08 +0900

    Update tests

--- test.rb
+++ test.rb
@@ -112,20 +112,48 @@ assert_equal 2, client[:animals].find(species: "bear").count
 
 assert_equal :bear, client[:animals].find({}, sort: { created_at: -1 }).first["species"]
 
-# Stringify all inputs / outputs
+# Store String instead of Symbol
 
-# for serialization
-class Symbol
-  def bson_type
-    BSON::String::BSON_TYPE
+begin
+  original_type = :foo.bson_type
+  $symbol_type = BSON::String::BSON_TYPE
+
+  class Symbol
+    def bson_type
+      $symbol_type
+    end
   end
+
+  lion = SymbolAnimal.create(species: :lion)
+  assert_equal :lion, lion.species
+  assert_equal :lion, lion.attributes["species"]
+  assert_match /species: :lion/, lion.inspect
+
+  lion_reloaded = SymbolAnimal.find(lion.id)
+  assert_equal :lion, lion_reloaded.species
+  assert_equal "lion", lion_reloaded.attributes["species"]
+  assert_match /species: "lion"/, lion_reloaded.inspect
+
+  lion_other = StringAnimal.find(lion.id)
+  assert_equal "lion", lion_other.species
+  assert_equal "lion", lion_other.attributes["species"]
+  assert_match /species: "lion"/, lion_other.inspect
+
+ensure
+  $symbol_type = original_type
 end
 
-# for deserialization
-BSON::Registry.register(BSON::Symbol::BSON_TYPE, ::String)
+# Retrieve String instead of Symbol
 
-assert_match /species: "cat"/, SymbolAnimal.find(cat.id).inspect
-assert_match /species: "dog"/, SymbolAnimal.find(dog.id).inspect
-assert_match /species: "tori"/, SymbolAnimal.find(bird.id).inspect
-assert_match /species: "rabbit"/, SymbolAnimal.find(rabbit.id).inspect
-assert_equal "bear", client[:animals].find({}, sort: { created_at: -1 }).first["species"]
+begin
+  original_type = BSON::Registry.get(BSON::Symbol::BSON_TYPE)
+  BSON::Registry.register(BSON::Symbol::BSON_TYPE, ::String)
+
+  assert_match /species: "cat"/, SymbolAnimal.find(cat.id).inspect
+  assert_match /species: "dog"/, SymbolAnimal.find(dog.id).inspect
+  assert_match /species: "tori"/, SymbolAnimal.find(bird.id).inspect
+  assert_match /species: "rabbit"/, SymbolAnimal.find(rabbit.id).inspect
+  assert_match /species: "lion"/, SymbolAnimal.find(lion.id).inspect
+ensure
+  BSON::Registry.register(BSON::Symbol::BSON_TYPE, original_type)
+end
